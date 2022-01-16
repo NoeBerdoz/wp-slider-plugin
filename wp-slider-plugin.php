71294286aeb64c27d21c5829f1508da9df3049e1
Noe <Noe.BERDOZ@cpnv.ch>
2022-01-16 20:52:03 +0100

    Plugin definition

---
+++ wp-slider-plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Plugin Name: WP Slider Plugin
+ * Plugin URI: none
+ * Description: A slider for WordPress
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: NoeBerdoz
+ * Author URI: https://github.com/NoeBerdoz
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-slider-plugin
+ * Domain Path: /languages
+ */
+
+/*
+WP Slider Plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+WP Slider Plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
