3f44b01b0bb1fbd6a8b946df657960fa2c8fe394
Noe <Noe.BERDOZ@cpnv.ch>
2022-01-31 10:06:02 +0100

    Add dynamics data to slider

--- views/wp-slider-plugin_shortcode.php
+++ views/wp-slider-plugin_shortcode.php
@@ -4,20 +4,46 @@
 </h3>
 <div class="wp-slider-plugin flexslider">
     <ul class="slides">
+    <?php
+        $args = array(
+                'post_type' => 'wp-slider-plugin',
+                'post_status' => 'publish',
+                'post_in' => $id,
+                'orderby' => $orderby
+        );
+
+        $my_query = new WP_Query($args);
+
+        if($my_query->have_posts()):
+            while($my_query->have_posts()): $my_query->the_post();
+
+            $button_text = get_post_meta(get_the_ID(), 'wp_slider_plugin_link_text', true);
+            $button_link = get_post_meta(get_the_ID(), 'wp_slider_plugin_link_url', true);
+    ?>
         <li>
+        <?php
+            // Add image as flexlider says
+            the_post_thumbnail('full', array('class' => 'img-fluid'));
+        ?>
             <div class="mvs-container">
                 <div class="slider-details-container">
                     <div class="wrapper">
                         <div class="slider-title">
-                            <h2>Slider Title</h2>
+                            <h2><?php the_title() ?></h2>
                         </div>
                         <div class="slider-description">
-                            <div class="subtitle">Subtitle</div>
-                            <a class="link" href="#">Button text</a>
+                            <div class="subtitle"><?php the_content(); ?></div>
+                            <a class="link" href="<?php echo esc_url($button_link); ?>"><?php echo esc_html($button_text); ?></a>
                         </div>
                     </div>
                 </div>
             </div>
         </li>
+    <?php
+        endwhile;
+        // Prevent interferences with other wp queries
+            wp_reset_postdata();
+        endif;
+    ?>
     </ul>
 </div>
